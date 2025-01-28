e4e32153979d1e8761a0e9a4d2d3881ac78ec125
IftekharAlamMithu <iftekharalammithu23@gmail.com>
2025-01-29 00:14:53 +0600

    `feat: add support for task lists, tables, and images to the editor`

--- src/app/documents/editor.tsx
+++ src/app/documents/editor.tsx
@@ -3,12 +3,39 @@
 import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import Document from "@tiptap/extension-document";
-import { bulletList } from "@tiptap/pm/schema-list";
+import TaskList from "@tiptap/extension-task-list";
+import TaskItem from "@tiptap/extension-task-item";
+import BulletList from "@tiptap/extension-bullet-list";
+import Table from "@tiptap/extension-table";
+import TableHeader from "@tiptap/extension-table-header";
+import TableRow from "@tiptap/extension-table-row";
+import TableCell from "@tiptap/extension-table-cell";
+import Image from "@tiptap/extension-image";
 
 const Editor = () => {
   const editor = useEditor({
-    extensions: [StarterKit, bulletList],
-    content: "<p>Hello World! 🌎️</p>",
+    extensions: [
+      StarterKit,
+      Document,
+      BulletList,
+      TaskList,
+      TaskItem.configure({
+        nested: true,
+      }),
+      Table.configure({
+        resizable: true,
+      }),
+      TableRow,
+      TableHeader,
+      TableCell,
+      Image,
+    ],
+    content: `
+        <p>This is a basic example of implementing images. Drag to re-order.</p>
+        <img src="https://placehold.co/800x400" />
+        <img src="https://placehold.co/800x400/6A00F5/white" />
+      `,
+    // content: "<p>Hello World! 🌎️</p>",
     editorProps: {
       attributes: {
         class:
